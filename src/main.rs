e9eab91e4901320e04869d5a5238bf17845f0cf1
Renato Cunha <cunhamrenato@gmail.com>
2024-09-24 12:28:35 -0300

    .

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,18 @@ use tokio::io::AsyncReadExt;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    let mut ip = String::new();
+
+    println!("Host on: ");
+    std::io::stdin().read_line(&mut ip).unwrap();
+
+    let listener = TcpListener::bind(&ip.trim()).await?;
     println!("Yapping server is now running!");
 
     loop {
         let (mut socket, _) = listener.accept().await?;
+        println!("Accepted connection!");
+
         tokio::spawn(async move {
             let mut buf = [0; 1024];
 
@@ -15,7 +22,6 @@ async fn main() -> std::io::Result<()> {
                 Ok(n) if n == 0 => return, 
                 Ok(n) => {
                     println!("Received: {}", String::from_utf8_lossy(&buf[..n]));
-                    // socket.write_all(&buf[0..n]).await.unwrap();
                 }
                 Err(e) => {
                     eprintln!("Failed to read from socket: {:?}", e);
